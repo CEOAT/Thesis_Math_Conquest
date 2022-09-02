95ee152ac0710a1215cca473a202f6af1cf2fc2f
wai713952 <wai.test@mail.kmutt.ac.th>
2022-09-03 02:50:42 +0700

    move 2D parallax scene to Master branch, change scripts name
    - move Mark's 2D parallax scene to Master branch
    - rename some scripts to match naming convention
    - organize test and demo folder

---
+++ Unity Project/Tilemap/Tilemap/Assets/Scenes/DungeonModeStageParallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DungeonModeStageParallax : MonoBehaviour
+{
+	public float speed;
+	private float x;
+	public float PontoDeDestino;
+	public float PontoOriginal;
+	void Start()
+	{
+		
+	}
+	void Update()
+	{
+		x = transform.position.x;
+		x += speed * Time.deltaTime;
+		transform.position = new Vector3(x, transform.position.y, transform.position.z);
+		if (x <= PontoDeDestino)
+		{
+			x = PontoOriginal;
+			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+		}
+	}
+}
