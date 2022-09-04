fe053532a702dbea21c94e0fcb3622a3caff2ada
wai713952 <wai.test@mail.kmutt.ac.th>
2022-09-04 17:20:23 +0700

    rework target system (WIP), re-adding tag and layer
    - add missing tag and layer, "Enemy", "Ground", "Player"
    - rename folder "DM Player Object Script" to "DM Player's Object Script"
    - [WIP] Dungeon Mode Target System, (can add/remove enemy to/from list by fix repeated active time, can create selection mark and removed after the enemy has been destroyed and out of target range)
    - [Problem] default material and sprite from 2D project is missing

--- Unity Project/Math Conquest 3D (Core)/Assets/Script/mode dungeon script/DM Player Control Script/DungeonModePlayerControllerTargetSystem.cs
+++ Unity Project/Math Conquest 3D (Core)/Assets/Script/mode dungeon script/DM Player Control Script/DungeonModePlayerControllerTargetSystem.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class DungeonModePlayerControllerTargetSystem : MonoBehaviour
 {
     [SerializeField]private List<Transform> enemyList = new List<Transform>();
+    private LayerMask playerAttackLayerMask;
+    private DungeonModePlayerControllerAttack AttackSystem;
 
     public GameObject selectionMarkPrefab;
     private GameObject selectionMarkObject;
+
     public Transform selectedEnemyObject;
     [SerializeField] private int selectedEnemyIndex;
 
@@ -17,23 +20,94 @@ public class DungeonModePlayerControllerTargetSystem : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating("TargetEnemyInArea", 0.5f, 0.5f);
+        SetupComponent();
+        InvokeRepeating("TargetEnemy", 0.5f, 0.5f);
     }
+    private void SetupComponent()
+    {
+        AttackSystem = GetComponent<DungeonModePlayerControllerAttack>();
+        playerAttackLayerMask = AttackSystem.playerAttackLayerMask;
+    }
+
     private void OnDrawGizmosSelected()
     {
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(new Vector3(targetAreaCenterPointObject.position.x,targetAreaCenterPointObject.position.y, 0),
-            new Vector3(targetAreaXValue, targetAreaYValue, 0.5f));
+            new Vector3(targetAreaXValue, targetAreaYValue, 0f));
     }
-    private void TargetEnemyInArea()
+    private void TargetEnemy()
     {
-        print("repeating");
+        CheckEnemyInArea();
+        CheckEnemyIndex();
+        CreateSelectionMark();
     }
+    private void CheckEnemyInArea()
+    {
+        Collider2D[] enemiesInTargetArea = Physics2D.OverlapBoxAll(
+            new Vector2(targetAreaCenterPointObject.position.x, targetAreaCenterPointObject.position.y),    //center point of gizmos (camera)
+            new Vector2(targetAreaXValue, targetAreaYValue),                                                //size of gizmos
+            0f,                                                                                             //angle of box
+            playerAttackLayerMask);                                                                         //get mask from player attack script ("enemy" layer)
 
+        foreach (Collider2D enemy in enemiesInTargetArea)
+        {
+            if (enemyList.Count == 0)
+            {
+                enemyList.Add(enemy.transform);
+            }
+            if (enemyList.Count > 0)
+            {
+                if (enemyList.Contains(enemy.transform) == false)
+                {
+                    enemyList.Add(enemy.transform);
+                }
+            }
+        }
 
+        if (enemiesInTargetArea.Length < enemyList.Count)
+        {
+            int enemyListCount = enemyList.Count;
+            for (int i = enemyListCount; i > 0; i--)
+            {
+                if (enemiesInTargetArea.Equals(enemyList[i - 1].transform) == false)
+                {
+                    enemyList.Remove(enemyList[i - 1].transform);
+                }
+            }
+        }
+    }
     private void CheckEnemyIndex()  //keep checking index everytime enemy enter-leave trigger
     {
-        selectedEnemyIndex = enemyList.IndexOf(selectedEnemyObject);
+        if (enemyList.Count == 0)
+        {
+            selectedEnemyIndex = -1;
+        }
+        else if (enemyList.Count == 1)
+        {
+            selectedEnemyIndex = 0;
+        }
     }
+    private void CreateSelectionMark()
+    {
+        if (enemyList.Count == 0)
+        {
+            if (selectionMarkObject != null)
+            {
+                selectedEnemyObject = null;
+                Destroy(selectionMarkObject);
+            }
+        }
+        else if (enemyList.Count == 1)
+        {
+            if (selectionMarkObject == null)
+            {
+                selectedEnemyObject = enemyList[0].transform;
+                selectionMarkObject = Instantiate(selectionMarkPrefab);
+                selectionMarkObject.GetComponent<ObjectSelectionMark>().enemyTransform = selectedEnemyObject;
+            }
+        }
+    }
+
     private void SwitchEnemy()  //can be pressed when there 2 more target
     {
         //increase index number
@@ -46,9 +120,9 @@ public class DungeonModePlayerControllerTargetSystem : MonoBehaviour
         }
         else if (enemyList.Count > 1)
         {
-            if (selectedEnemyIndex + 2 > enemyList.Count)
+            if (selectedEnemyIndex + 1 > enemyList.Count)
             {
-                selectedEnemyIndex++;
+                selectedEnemyIndex = 0;
                 return;
             }
             selectedEnemyIndex++;
