605b2387bc067e9cdb64f92724c3652322e412d6
wai713952 <wai.test@mail.kmutt.ac.th>
2022-09-25 00:23:02 +0700

    Add puzzle window system
    - Add puzzle window system (can link with puzzle script to regenerate new question after answer the question in window)
    - Organize scene folder (delete blank scene)
    - Added color sprite demo example

--- Unity Project/Math Conquest 3D (Core)/Assets/Script/mode exploration script/EM object/ExplorationModeObjectInteractableWindow.cs
+++ Unity Project/Math Conquest 3D (Core)/Assets/Script/mode exploration script/EM object/ExplorationModeObjectInteractableWindow.cs
@@ -1,27 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ExplorationModeObjectInteractableWindow : MonoBehaviour
 {
     public GameObject windowGroup;
+    public TMP_Text windowTextPuzzle;
+    public TMP_Text windowTextDescription;
+    public TMP_InputField windowInputField;
+
+    public string windowAnswer;
+    public bool isWindowReset;
+
+    private MasterInput playerInput;
 
     private void OnEnable()
     {
-        ExplorationModeObjectInteractable.interaction += WindowActivation;
+        playerInput.Enable();
+        ExplorationModeObjectInteractable.puzzleInteract += WindowActivation;
     }
     private void OnDisable()
     {
-        ExplorationModeObjectInteractable.interaction -= WindowActivation;
+        playerInput.Disable();
+        ExplorationModeObjectInteractable.puzzleInteract -= WindowActivation;
     }
 
     private void Awake()
     {
+        SetupComponent();
+        SetupControl();
         SetupObject();
     }
     private void SetupObject()
     {
         windowGroup.SetActive(false);
+    }
+    private void SetupComponent()
+    {
+        playerInput = new MasterInput();
+        windowInputField.GetComponent<TMP_InputField>();
+    }
+    private void SetupControl()
+    {
+        playerInput.WindowControl.CloseWindow.performed += context => CloseWindow();
+        playerInput.WindowControl.ConfirmAnswer.performed += context => ConfirmAnswer();
+    }
+
+    public void SetupWindow(string textPuzzle, string textDescription, string puzzleAnswer)
+    {
+        windowTextPuzzle.text = textPuzzle;
+        windowTextDescription.text = textDescription;
+        windowAnswer = puzzleAnswer;
     }
 
     private void WindowActivation()
@@ -32,13 +62,29 @@ public class ExplorationModeObjectInteractableWindow : MonoBehaviour
         }
         else if (windowGroup.activeInHierarchy == true)
         {
-            windowGroup.SetActive(false);
+            CloseWindow();
         }
     }
-
-    public void ConfirmAnswer()
+    private void CloseWindow()
     {
-        
+        windowGroup.SetActive(false);
+    }
+    private void ConfirmAnswer()
+    {
+        if (windowInputField.isActiveAndEnabled == true)
+        {
+            if (windowInputField.text.ToString().ToUpper() == windowAnswer && isWindowReset == false)
+            {
+                print("correct");
+                isWindowReset = true;
+            }
+        }
+        windowInputField.text = "";
+
     }
 
+    private void FixedUpdate()
+    {
+        windowInputField.ActivateInputField();
+    }
 }
