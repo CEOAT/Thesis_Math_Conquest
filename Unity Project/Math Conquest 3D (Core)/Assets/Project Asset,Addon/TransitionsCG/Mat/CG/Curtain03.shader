c2a6766f719b65498695af2ed487743fb98925ea
poomwarning <poomwarning08@gmail.com>
2022-12-06 03:18:51 +0700

    -Add MainMenu Transition Shader

---
+++ Unity Project/Math Conquest 3D (Core)/Assets/Project Asset,Addon/TransitionsCG/Mat/CG/Curtain03.shader
@@ -0,0 +1,80 @@
+﻿Shader "Jettelly/Curtain03"
+{
+    Properties
+    {
+        _MainTex ("Sprite Texture", 2D) = "white" {}
+        _Cutoff ("Cutoff", Range(0, 1)) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "Queue"="Transparent"}
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float2 textureuv : TEXCOORD1;
+                float4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float2 textureuv : TEXCOORD1;
+                float4 vertex : SV_POSITION;
+                float4 color : COLOR;
+            };   
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _Cutoff;         
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.color = v.color;
+                o.textureuv = TRANSFORM_TEX(v.uv,_MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                 half4 colortex = tex2D(_MainTex, i.uv)  * i.color;
+                float leftCurtain = i.uv.y;
+                float rightCurtain = 1 - i.uv.y;
+                float color = 0;
+
+                if(i.uv.x < 0.5)
+                {
+                    color = leftCurtain;
+                    
+                }
+                else
+                {
+                    color = rightCurtain;
+                }
+
+                if(color < _Cutoff)
+                {
+                    return i.color  *colortex;;
+                }
+                else
+                {
+                    return float4(0, 0, 0, 0);
+                }
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Unity Project/Math Conquest 3D (Core)/Assets/Project Asset,Addon/TransitionsCG/Mat/CG/CurtainController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CurtainController : MonoBehaviour
+{
+    [SerializeField] private Image color_01;
+    [SerializeField] private string curtainSceneName;
+    [SerializeField] private float curtainSpeed;
+    [SerializeField] private bool hasEnter;
+    private float radiusColor_01;
+    private bool enter;    
+    
+    // Start is called before the first frame update
+    void Start()
+    {        
+        Initialize();        
+    }
+
+    private void Initialize()
+    {
+        if (!hasEnter)
+        {
+            if (color_01 != null)
+            {      
+                radiusColor_01 = 0; 
+                color_01.material.SetFloat("_Cutoff", radiusColor_01); // UP TO 1     
+            }
+        }
+        else
+        {
+             if (color_01 != null)
+            {       
+                radiusColor_01 = 1;
+                color_01.material.SetFloat("_Cutoff", radiusColor_01); 
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            StartCoroutine(ActiveCanvas());
+        }
+
+        if (enter &&  color_01.material.GetFloat("_Cutoff") <= 1)
+        {
+            radiusColor_01 += Time.deltaTime * curtainSpeed;
+            color_01.material.SetFloat("_Cutoff", radiusColor_01); // UP TO 1     
+        }
+
+        if (hasEnter && color_01.material.GetFloat("_Cutoff") >= 0)
+        {
+            radiusColor_01 -= Time.deltaTime * curtainSpeed;
+            color_01.material.SetFloat("_Cutoff", radiusColor_01); // UP TO 1    
+
+            if(color_01.material.GetFloat("_Cutoff") <= 0) 
+            {
+                color_01.material.SetFloat("_Cutoff", 0);   
+                hasEnter = false;    
+            }
+        }       
+    }
+
+    IEnumerator ActiveCanvas()
+    {        
+        enter = true;
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(curtainSceneName);
+    }
+}
