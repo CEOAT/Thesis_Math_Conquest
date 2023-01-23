a35fc2005b5c7b08229aabd4f8167ba99aa03db0
poomwarning <poomwarning08@gmail.com>
2023-01-23 14:00:05 +0700

    -fix animation button event
    Missing keyframe interpolation between different button stage

--- Unity Project/Math Conquest 3D (Core)/Assets/Script/general script/UI script/MainMenuManager.cs
+++ Unity Project/Math Conquest 3D (Core)/Assets/Script/general script/UI script/MainMenuManager.cs
@@ -1,42 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public Image transistion;
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public struct MathConButtonMenuNavigation
     {
-       
+        public string ButtonAction;
+        public float TransistionDelay;
+        public Button buttonClick;
+        public GameObject From;
+        public GameObject To;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float tempDelay;
+    [SerializeField] private MathConButtonMenuNavigation[] ButtonEvents;
+    public Image transistion;
 
     public void playtransistion()
+    {
+        foreach (var B_Event in ButtonEvents)
+        {
+            if (B_Event.buttonClick == EventSystem.current.currentSelectedGameObject.GetComponent<Button>())
+            {
+                tempDelay = B_Event.TransistionDelay;
+                TransitionMaterialChange();
+                StartCoroutine(ChangeCanvas(B_Event));
+            } ;
+        }
+    }
+
+    public void TransitionMaterialChange()
     {
         transistion.raycastTarget = false;
         StartCoroutine(ChangeTransistionMaterial());
     }
 
+    IEnumerator ChangeCanvas(MathConButtonMenuNavigation buttonEvent)
+    {
+        buttonEvent.From.TryGetComponent(out GraphicRaycaster FromRaycast);
+        FromRaycast.enabled = false;
+        yield return new WaitForSeconds(tempDelay+1f);
+        buttonEvent.From.SetActive(false);
+        FromRaycast.enabled = true;
+        buttonEvent.To.SetActive(true);
+        yield break;
+    }
+
     IEnumerator ChangeTransistionMaterial()
     {
         transistion.raycastTarget = true;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.25f);
         Material tempmat = transistion.material;
-        float t = 0;
         Sequence s = DOTween.Sequence();
-        s.Append(DOVirtual.Float(0, 1f, 1f, v => tempmat.SetFloat("_Cutoff",v )));
-        yield return new WaitForSeconds(1.25f);
-        s.Append(DOVirtual.Float(1F, 0f, 1f, v => tempmat.SetFloat("_Cutoff",v )));
+        s.Append(DOVirtual.Float(0, 1f, tempDelay, v => tempmat.SetFloat("_Cutoff",v )));
+        yield return new WaitForSeconds(2f);
+        s.Append(DOVirtual.Float(1F, 0f, tempDelay, v => tempmat.SetFloat("_Cutoff",v )));
         transistion.raycastTarget = false;
-       
         yield break;
     }
 }
