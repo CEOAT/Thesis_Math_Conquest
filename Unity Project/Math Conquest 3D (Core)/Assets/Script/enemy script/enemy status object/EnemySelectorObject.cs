82346beb8df18ed6bd5fd4d319284d936a0e75ac
wai713952 <wai.test@mail.kmutt.ac.th>
2023-04-24 18:35:09 +0700

    Add sound effect and new bgm
    - Fix blackbar animation to transition between active and deactive, prevent stutter during cutscene to cutscene trigger
    - Add sfx to slime, stone, item and dead effect

--- Unity Project/Math Conquest 3D (Core)/Assets/Script/enemy script/enemy status object/EnemySelectorObject.cs
+++ Unity Project/Math Conquest 3D (Core)/Assets/Script/enemy script/enemy status object/EnemySelectorObject.cs
@@ -6,6 +6,12 @@ public class EnemySelectorObject : MonoBehaviour
 {
     public Transform selectorTrackPoint;
     public float selectorRotateSpeed;
+    private void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * selectorTrackPoint.localScale.x
+                                            , transform.localScale.y * selectorTrackPoint.localScale.y
+                                            , transform.localScale.z * selectorTrackPoint.localScale.z);
+    }
     private void FixedUpdate()
     {
         transform.position = selectorTrackPoint.position + new Vector3(0,0,0);
